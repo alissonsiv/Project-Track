247b0f57d9508cfb7f36e665b3350abf0ba66dff
Alisson <alissonmoura890@gmail.com>
2025-07-10 08:43:22 -0300

    Adicionar Header (Navbar) e Footer ao layout da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
-import Container from "./components/pages/layout/Container";
+import Container from "./components/layout/Container";
+import Projects from "./components/pages/Projects"
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/company">Company</Link> |{" "}
-          <Link to="/contact">Contact</Link> |{" "}
-          <Link to="/newproject">New</Link>
-        </nav>
-      </header>
+      <Navbar />
 
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-
-      <footer>
-        <p>Footer</p>
-      </footer>
+      <Footer />
     </BrowserRouter>
   );
 }

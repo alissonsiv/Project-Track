4d84e52ce47aa6b0cdd330e9ece6415281010c26
Alisson <alissonmoura890@gmail.com>
2025-07-15 19:02:35 -0300

    Configuração das chamadas à API

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,26 +1,42 @@
+import { useEffect, useState } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css"
 
-function ProjectForm({btnText}) {
+function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((resp) => resp.json())
+            .then((data) => setCategories(data))
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <form>
             <Input
-            type="text"
-            text="Nome do projeto"
-            name="name"
-            placeholder="Insira o nome do projeto"
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
             />
             <Input
-            type="number"
-            text="Orçamento do proejto"
-            name="budget"
-            placeholder="Insira o orçamento total"
+                type="number"
+                text="Orçamento do proejto"
+                name="budget"
+                placeholder="Insira o orçamento total"
             />
 
-            <Select name="category_id" text="Selecione a categoria" />
-            <SubmitButton text={btnText}/>
+            <Select name="category_id" text="Selecione a categoria" options={categories} />
+            <SubmitButton text={btnText} />
         </form>
     );
 }
